7e28f4f2a7d7f7a8e47165cc6561875c48c71330
Nat <nathaliasalinas97@gmail.com>
2024-02-19 22:24:34 -0500

    added tkinter GUI. Buttons for Rock, Paper, Scissor selections, a 'Shoot' button to confirm selection, and two textboxes to display what player 1 and the computer selected

--- RPS.py
+++ RPS.py
@@ -1,4 +1,5 @@
 import random
+import tkinter as tk
 
 def play():
     user = input("What is your choice?\n'r' for rock, 'p' for paper, 's' for scissors\n")
@@ -19,4 +20,44 @@ def is_win(player, opponent):
     if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
 
-print(play())
+window = tk.Tk() 
+window.geometry("600x300")
+window.title("Rock, Paper, Scissors!")
+
+label = tk.Label(window, text="Select your choice", font=('Arial',18))
+label.grid(row=0)
+
+r_button = tk.Button(window, text="Rock", width=10, font=('Arial', 18))
+r_button.grid(row=1, column=0, pady=20)
+
+p_button = tk.Button(window, text="Paper", width=10, font=('Arial', 18))
+p_button.grid(row=1, column=1, padx=10, pady=20)
+
+s_button = tk.Button(window, text="Scissors", width=10, font=('Arial', 18))
+s_button.grid(row=1, column=2, padx=10, pady=20)
+
+shoot_button = tk.Button(window, text="Shoot!", width=10, font=('Arial', 18), highlightbackground='green', fg='green')
+shoot_button.grid(row=7, column=1, pady=20)
+
+# textbox to hold generated password 
+p1_textbox = tk.Text(window, height=1, width=20, font=('Arial', 16))
+p1_textbox.grid(row=4, column=0, padx=5)
+
+p1_label = tk.Label(window, text="Player 1", font=('Arial',18))
+p1_label.grid(row=5, column=0)
+
+vs_label = tk.Label(window, text="VS", font=('Arial',18))
+vs_label.grid(row=4, column=1)
+
+# textbox to hold generated password 
+ai_textbox = tk.Text(window, height=1, width=20, font=('Arial', 16))
+ai_textbox.grid(row=4, column=2, padx=5)
+
+ai_label = tk.Label(window, text="Computer", font=('Arial',18))
+ai_label.grid(row=5, column=2)
+
+
+# print(play())
+
+
+window.mainloop()
