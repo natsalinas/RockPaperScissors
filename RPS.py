ae5a0298e8b503fb4151366dca94230d761aca62
Nat <nathaliasalinas97@gmail.com>
2024-02-21 23:29:53 -0500

    created function to determine which button is pressed (Rock, Paper, or Scissors)

--- RPS.py
+++ RPS.py
@@ -1,6 +1,10 @@
 import random
 import tkinter as tk
 
+p1_rock = 0
+p1_paper = 0 
+p1_scissors = 0 
+
 def play():
     user = input("What is your choice?\n'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
@@ -19,6 +23,22 @@ def is_win(player, opponent):
     # r > s, s > p, p > r
     if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+    
+# if button is clicked, the selected "choice" is displayed on the text field. 
+# Define function to get the information about the Button
+def press_button(t):
+    if t == 1:
+        print("You selected Rock")
+    elif t == 2:
+        print("You selected Paper")
+    elif t == 3:
+        print("You selected Scissors")
+
+#def update_p1(tk.Text, string):
+#   tk.Text.insert(tk.END, string)
+
+
+
 
 window = tk.Tk() 
 window.geometry("600x300")
@@ -27,21 +47,25 @@ window.title("Rock, Paper, Scissors!")
 label = tk.Label(window, text="Select your choice", font=('Arial',18))
 label.grid(row=0)
 
-r_button = tk.Button(window, text="Rock", width=10, font=('Arial', 18))
-r_button.grid(row=1, column=0, pady=20)
-
-p_button = tk.Button(window, text="Paper", width=10, font=('Arial', 18))
-p_button.grid(row=1, column=1, padx=10, pady=20)
-
-s_button = tk.Button(window, text="Scissors", width=10, font=('Arial', 18))
-s_button.grid(row=1, column=2, padx=10, pady=20)
-
-shoot_button = tk.Button(window, text="Shoot!", width=10, font=('Arial', 18), highlightbackground='green', fg='green')
-shoot_button.grid(row=7, column=1, pady=20)
-
 # textbox to hold generated password 
 p1_textbox = tk.Text(window, height=1, width=20, font=('Arial', 16))
 p1_textbox.grid(row=4, column=0, padx=5)
+
+
+r_button = tk.Button(window, text="Rock", width=10, font=('Arial', 18), command=lambda t= 1: press_button(t))
+r_button.grid(row=1, column=0, pady=20)
+if (p1_rock == 1):
+    p1_textbox.insert(tk.END, "Rock")
+
+
+p_button = tk.Button(window, text="Paper", width=10, font=('Arial', 18), command=lambda t= 2: press_button(t))
+p_button.grid(row=1, column=1, padx=10, pady=20)
+
+s_button = tk.Button(window, text="Scissors", width=10, font=('Arial', 18), command=lambda t= 3: press_button(t))
+s_button.grid(row=1, column=2, padx=10, pady=20)
+
+shoot_button = tk.Button(window, text="Shoot!", width=10, font=('Arial', 18), highlightbackground='green', fg='green', command=lambda t= "Shoot Clicked": press_button(t))
+shoot_button.grid(row=7, column=1, pady=20)
 
 p1_label = tk.Label(window, text="Player 1", font=('Arial',18))
 p1_label.grid(row=5, column=0)
